e7029a01feacd2c3a9849a865b5b3f68eb2c3ca4
LogWat <Nk8877wpt+Karan@gmail.com>
2021-09-12 23:04:23 +0900

    デバッグ用

--- src/lib.rs
+++ src/lib.rs
@@ -7,36 +7,11 @@ use winapi::um::winuser::{MB_OK, MessageBoxW};
 use winapi::shared::minwindef::*;
 
 #[no_mangle]
-pub unsafe extern "system" fn DllMain(_: HINSTANCE, reason: u32, _: u32) -> BOOL {
-    match reason {
-        1 => {
-            let s = "DLL_PROCESS_ATTACH\0".to_string();
-            msg(&s);
-        }
-        _ => {
-            let s = "DEFAULT\0".to_string();
-            msg(&s);
-        }
-    }
-    TRUE
-}
-
-#[no_mangle]
-pub extern "C" fn hello() {
-    let s = "hello\0".to_string();
-    msg(&s);
-}
-
-fn msg(caption: &str) {
-    let lp_text: Vec<u16> = "Hello World!!\0".encode_utf16().collect();
+pub unsafe extern "stdcall" fn DllMain(_: HINSTANCE, reason: u32, _: u32) -> i32 {
+    let lp_text: Vec<u16> = "Hello, World!!\0".encode_utf16().collect();
+    let caption = "hello\0".to_string();
     let lp_caption: Vec<u16> = caption.encode_utf16().collect();
 
-    unsafe {
-        MessageBoxW(
-            std::ptr::null_mut(),
-            lp_text.as_ptr(),
-            lp_caption.as_ptr(),
-            MB_OK
-        );
-    }
+    MessageBoxW(std::ptr::null_mut(), lp_text.as_ptr(), lp_caption.as_ptr(), MB_OK);
+    0
 }
