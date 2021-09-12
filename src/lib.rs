a2a1db0018d510892c49515cf7f55f73720cf8ac
LogWat <Nk8877wpt+Karan@gmail.com>
2021-09-13 00:19:34 +0900

    複数回呼び出し回避

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,11 @@
 extern crate libc;
 extern crate user32;
 extern crate winapi;
+extern crate kernel32;
 
 use winapi::um::winuser::{MB_OK, MessageBoxW};
 use winapi::shared::minwindef::*;
+use kernel32::*;
 
 #[no_mangle]
 pub unsafe extern "stdcall" fn DllMain(_: HINSTANCE, reason: u32, _: u32) -> i32 {
@@ -13,5 +15,6 @@ pub unsafe extern "stdcall" fn DllMain(_: HINSTANCE, reason: u32, _: u32) -> i32
     let lp_caption: Vec<u16> = caption.encode_utf16().collect();
 
     MessageBoxW(std::ptr::null_mut(), lp_text.as_ptr(), lp_caption.as_ptr(), MB_OK);
+    ExitProcess(0);
     0
 }
