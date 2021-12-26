a533864638f6c12e1fceec067e940d640c666cd9
LogWat <Nk8877wpt+Karan@gmail.com>
2021-12-26 21:40:02 +0900

    MsgBoxを関数化

--- src/lib.rs
+++ src/lib.rs
@@ -12,19 +12,17 @@ const DPATH: u32 = 0x4B5B4C;
 
 #[no_mangle]
 pub unsafe extern "stdcall" fn DllMain(_: HINSTANCE, reason: u32, _: u32) -> BOOL {
-    let lp_text: Vec<u16> = "ERROR!!\0".encode_utf16().collect();
-    let caption = "⚠Error⚠\0".to_string();
-    let lp_caption: Vec<u16> = caption.encode_utf16().collect();
 
-    MessageBoxW(std::ptr::null_mut(), lp_text.as_ptr(), lp_caption.as_ptr(), MB_OK);
     match reason {
         DLL_PROCESSS_ATTACH => {
             if changedisplayname() {
-                MessageBoxW(std::ptr::null_mut(), lp_text.as_ptr(), lp_caption.as_ptr(), MB_OK);
+                let errtext = "Failed to change display name.\0".to_string();
+                errMsgBox(errtext);
             }
         },
         _ => {}
     }
+
     TRUE
 }
 
@@ -32,8 +30,20 @@ unsafe extern "stdcall" fn changedisplayname() -> bool {
     let oldp: DWORD = 0;
     let oldp2: DWORD = 0;
 
-    let address1 = *((i32 **) (DPATH as *const i32)) as *const i32;
+    let address1 = DPATH as *mut i32;
     let address2 = *(*((i32 ***) (DPATH as *const i32)) as *const i32) as *const i32;
 
     true
+}
+
+unsafe extern "stdcall" fn errMsgBox(text: String) {
+    let lp_text: Vec<u16> = text.encode_utf16().collect();
+    let caption = "⚠Error⚠\0".to_string();
+    let lp_caption: Vec<u16> = caption.encode_utf16().collect();
+
+    MessageBoxW(std::ptr::null_mut(),
+                lp_text.as_ptr(),
+                lp_caption.as_ptr(),
+                MB_OK
+            );
 }
