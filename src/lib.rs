502ca15f2ae28e70b8da53fab0e72ca62a0bb63f
LogWat <Nk8877wpt+Karan@gmail.com>
2022-01-11 17:59:00 +0900

    書き換え後権限修正&ポインタを数値に変更

--- src/lib.rs
+++ src/lib.rs
@@ -34,29 +34,41 @@ pub unsafe extern "stdcall" fn DllMain(_: HINSTANCE, reason: u32, _: u32) -> BOO
 // 生ポインタの利用 *mut or *const
 // この場合，アスタリスクは参照外しではなく型の一部である
 unsafe extern "stdcall" fn changedisplayname() -> bool {
-    let mut last_page: DWORD = 0;
 
-    let address2 = (*((*(DPATH as *mut i32) + 0x192C) as *mut i32)) as *mut i32; // [[0x4B5B4C] + 0x64B * 4]
-
+    let mut addr = *((*(DPATH as *mut i32) + 0x192C) as *mut i32); // [[0x4B5B4C] + 0x64B * 4]
     let num_of_characters = *(((*(DPATH as *mut i32)) + 0xCD4) as *mut i32); // [[0x4B5B4C] + 0x335 * 4]
 
-    let mut c = address2;
+    let mut last_page: DWORD = 0;
+
     for _ in 0..num_of_characters {
-        if *((c as i32 + 0x4) as *mut i32) == 0x7473694D && *((c as i32 + 0x8) as *mut i32) == 0x6E656B61 {
+        if *((addr + 0x4) as *mut i32) == 0x7473694D && *((addr + 0x8) as *mut i32) == 0x6E656B61 {
+
+            // 書き換えを行うために権限変更
             if VirtualProtect(
-                c as *mut _,
+                addr as *mut _,
                 ((size_of::<i32>() * 4) as u64).try_into().unwrap(),
                 PAGE_READWRITE,
                 &mut last_page
             ) == 0 {
                 return false;
             }
-            *((c as i32 + 0x4) as *mut i32) = 0x21212121;
-            *((c as i32 + 0x8) as *mut i32) = 0x21212121;
+
+            *((addr + 0x4) as *mut i32) = 0x21212121;
+            *((addr + 0x8) as *mut i32) = 0x21212121;
+
+            // 権限を元に戻す
+            if VirtualProtect(
+                addr as *mut _,
+                ((size_of::<i32>() * 4) as u64).try_into().unwrap(),
+                last_page,
+                &mut last_page
+            ) == 0 {
+                return false;
+            }
             
             return true;
         }
-        c = (c as i32 + 0x438) as *mut i32;
+        addr += 0x438;
     }
     false
 }
