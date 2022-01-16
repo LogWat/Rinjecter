dcf9b1f30780594193c8716bac6062359bb5809b
LogWat <Nk8877wpt+Karan@gmail.com>
2022-01-16 21:28:48 +0900

    VirtualQuery呼び出し用メソッド追加

--- src/processlib.rs
+++ src/processlib.rs
@@ -1,6 +1,7 @@
 use getset::Getters;
 use std::{mem, ffi::OsString, os::windows::ffi::OsStringExt};
 use winapi::um::{handleapi, memoryapi, processthreadsapi, tlhelp32, winnt};
+use winapi::shared::minwindef;
 
 #[derive(Getters)]
 #[get = "pub"]
@@ -24,7 +25,6 @@ pub struct Thread {
 }
 
 impl Process {
-    #[no_mangle]
     pub fn current_process() -> Self {
         let mut process = Self {
             pid: 0,
@@ -34,7 +34,32 @@ impl Process {
         process
     }
 
-    #[no_mangle]
+    // 別にExにする必要は無いけど一応汎用性を持たせるために
+    // 他のプロセスに対しても使えるようにしておく
+    pub fn check_protection(&self, address: u32) -> Result<minwindef::DWORD, &'static str> {
+        let mut meminfo = winnt::MEMORY_BASIC_INFORMATION {
+            BaseAddress: address as *mut _,
+            AllocationBase: address as *mut _,
+            AllocationProtect: 0,
+            RegionSize: 0,
+            State: 0,
+            Protect: 0,
+            Type: 0,
+        };
+        if unsafe {
+            memoryapi::VirtualQueryEx(
+                self.handle,
+                address as _,
+                &mut meminfo as *mut _ as _,
+                mem::size_of::<winnt::MEMORY_BASIC_INFORMATION>() as _,
+            )
+        } == 0 {
+            return Err("Failed to get memory info.");
+        }
+
+        Ok(meminfo.Protect)
+    }
+
     pub fn read_memory(&self, address: u32) -> Result<u32, &'static str> {
         let mut buffer = unsafe { mem::zeroed() }; 
         let mut bytes_read: libc::size_t = 0;
@@ -54,7 +79,6 @@ impl Process {
         Ok(buffer)
     }
 
-    #[no_mangle]
     pub fn get_module(&self, module_name: &str) -> Result<Module, &'static str> {
         let module = unsafe { 
             tlhelp32::CreateToolhelp32Snapshot(tlhelp32::TH32CS_SNAPMODULE, self.pid) 
@@ -88,39 +112,16 @@ impl Process {
 }
 
 impl Module {
-    pub fn read<T>(&self, offset: u32) -> Result<T, &'static str> {
-        let mut read = unsafe { mem::zeroed() };
-        let mut amount_read: libc::size_t = 0;
-
-        unsafe {
-            if memoryapi::ReadProcessMemory(
-                Process::current_process().handle,
-                (self.base_address + offset) as *mut _,
-                &mut read as *mut _ as *mut _,
-                mem::size_of::<T>() as _,
-                &mut amount_read as *mut _,
-            ) != (true as _) || amount_read == 0 {
-                return Err("Failed to read memory.");
-            }
-        }
-
-        Ok(read)
+    #[no_mangle]
+    pub fn fix_offset(&self, offset: u32) -> u32 {
+        self.base_address + offset
     }
 
-    pub fn write<T>(&self, offset: u32, value: T) -> Result<(), &'static str> {
-        let mut written: libc::size_t = 0;
-        unsafe {
-            if memoryapi::WriteProcessMemory(
-                Process::current_process().handle,
-                (self.base_address + offset) as *mut _,
-                &value as *const _ as *mut _,
-                mem::size_of::<T>() as _,
-                &mut written as *mut _,
-            ) != mem::size_of_val(&value) as i32 {
-                return Err("Failed to write memory.");
-            }
-        }
+    pub unsafe fn read<T>(&self, offset: u32) -> &T {
+        &*(self.fix_offset(offset) as *const T)
+    }
 
-        Ok(())
+    pub unsafe fn write<T>(&self, offset: u32, value: T) {
+        *(self.fix_offset(offset) as *mut T) = value;
     }
 }
