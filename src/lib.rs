cbf5e25a3999d5ca2f75af468c865a235edf0b52
LogWat <Nk8877wpt+Karan@gmail.com>
2022-01-16 22:47:01 +0900

    writeに置き換え

--- src/lib.rs
+++ src/lib.rs
@@ -28,8 +28,8 @@ pub extern "stdcall" fn DllMain(
         DLL_PROCESS_ATTACH => {
             unsafe {
                 libloaderapi::DisableThreadLibraryCalls(hinst_dll);
-                let mut process = Process::current_process();
-                if changedisplayname() == false {
+                let process = Process::current_process();
+                if changedisplayname(process) == false {
                     let errtext = "Failed to change display name.\0".to_string();
                     err_msgbox(errtext);
                 }
@@ -93,44 +93,29 @@ unsafe extern "stdcall" fn rewrite_program() -> Result<(), &'static str> {
 }
 
 // 生ポインタの利用 *mut or *const
-unsafe extern "stdcall" fn changedisplayname() -> bool {
+unsafe extern "stdcall" fn changedisplayname(process: Process) -> bool {
 
     let mut addr = *((*(DPATH as *mut i32) + 0x192C) as *mut i32); // [[0x4B5B4C] + 0x64B * 4]
     let num_of_characters = *(((*(DPATH as *mut i32)) + 0xCD4) as *mut i32); // [[0x4B5B4C] + 0x335 * 4]
 
-    let mut last_page: DWORD = 0;
     let names: Vec<&[u8]> = vec![b"Hello, UnderWorld!\0", b"\\(^o^)/\0", b"OXOXOXOXOXOXOXOXOXOXOXOXO\0", b" \0", b"OMFG! Miko!!!\0"];
 
     for _ in 0..num_of_characters {
         if *((addr + 0x4) as *mut i32) == 0x7473694D && *((addr + 0x8) as *mut i32) == 0x6E656B61 {
 
-            // 書き換えを行うために権限変更
-            if memoryapi::VirtualProtect(
-                addr as *mut _,
-                ((mem::size_of::<i32>() * 4) as u64).try_into().unwrap(),
-                PAGE_READWRITE,
-                &mut last_page
-            ) == 0 {
-                return false;
-            }
-
             // Nameを書き換え
             let mut rng = rand::thread_rng();
             let name_index = rng.gen_range(0..(names.len() - 1));
+
             for i in 0..names[name_index].len() {
-                *((addr + 0x4 + i as i32) as *mut u8) = names[name_index][i];
+                match Process::write(&process, (addr + 0x4 + i as i32) as u32, names[name_index][i]) {
+                    Ok(_) => {},
+                    Err(_) => {
+                        return false;
+                    }
+                }
             }
 
-            // 権限を元に戻す
-            if memoryapi::VirtualProtect(
-                addr as *mut _,
-                ((mem::size_of::<i32>() * 4) as u64).try_into().unwrap(),
-                last_page,
-                &mut last_page
-            ) == 0 {
-                return false;
-            }
-            
             return true;
         }
         addr += 0x438;
