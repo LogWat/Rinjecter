8722c2e7a7e1d3035a8cc969f8125c0954e8f59a
LogWat <Nk8877wpt+Karan@gmail.com>
2022-01-17 14:35:17 +0900

    書き込み幅修正

--- src/overwrite.rs
+++ src/overwrite.rs
@@ -7,9 +7,9 @@ pub unsafe extern "stdcall" fn overwrite(process: &Process) -> Result<(), &'stat
         0xEA000D8B, 0xEA00158B, 0xEA043589, 0xEA0405C7,
         0xEA04358B, 0xEA04158B, 0x4BEA04A1, 0xEA040D8B,
         0xEA041589, 0xEA083D83, 0x4BEA08A1, 0xEA083589,
-        0xEA0805C7, 0x4BEA08A3, 0xEA081589, 0xEA081539
+        0xEA0805C7, 0x4BEA08A3, 0xEA081589, 0xEA081589
         ];
-    let rb2: [u32; 3] = [0x9000, 0x4B, 0x1004B];
+    let rb2: [u16; 2] = [0x9000, 0x4B];
         
     // rewrite program
     Process::write(process, 0x41DBD4, rb1[0]).unwrap();     // -> mov eax, [0x4BEA00] ([0x4BEA00] = 0x0)
@@ -54,14 +54,14 @@ pub unsafe extern "stdcall" fn overwrite(process: &Process) -> Result<(), &'stat
     Process::write(process, 0x47BF21, rb2[1]).unwrap();
     Process::write(process, 0x41F8CE, rb1[7]).unwrap();    // -> mov [0x4BEA00], 0x0
     Process::write(process, 0x41F8D2, rb2[1]).unwrap();
-    Process::write(process, 0x41F8D6, 0x0 as u32).unwrap();
+    Process::write(process, 0x41F8D6, 0x0 as u16).unwrap();
     Process::write(process, 0x41F8ED, rb1[6]).unwrap();    // -> mov [0x4BEA00], esi
     Process::write(process, 0x41F8F1, rb2[1]).unwrap();
     Process::write(process, 0x41F90D, rb1[8]).unwrap();    // -> mov [0x4BEA00], eax
     Process::write(process, 0x41F911, rb2[0]).unwrap();    // -> nop
     Process::write(process, 0x41F9BD, rb1[7]).unwrap();
     Process::write(process, 0x41F9C1, rb2[1]).unwrap();
-    Process::write(process, 0x41F9C5, 0x0 as u32).unwrap();
+    Process::write(process, 0x41F9C5, 0x0 as u16).unwrap();
     Process::write(process, 0x4204A8, rb1[9]).unwrap();    // -> mov edx, [0x4BEA00]
     Process::write(process, 0x4204AC, rb2[1]).unwrap();
     Process::write(process, 0x420518, rb1[10]).unwrap();   // -> mov eax, [0x4BEA00]
@@ -80,14 +80,14 @@ pub unsafe extern "stdcall" fn overwrite(process: &Process) -> Result<(), &'stat
     Process::write(process, 0x42E911, rb2[1]).unwrap();
     Process::write(process, 0x41DD98, rb1[13]).unwrap();   // -> cmp [0x4BEA00], 0x3
     Process::write(process, 0x41DD9C, rb2[1]).unwrap();
-    Process::write(process, 0x41DD9E, 0x3 as u32).unwrap();
+    Process::write(process, 0x41DD9E, 0x3 as u8).unwrap();
     Process::write(process, 0x41DF50, rb1[14]).unwrap();   // -> mov eax, [0x4BEA00]
     Process::write(process, 0x41DF54, rb2[0]).unwrap();    // -> nop
     Process::write(process, 0x41F8C3, rb1[15]).unwrap();   // -> mov [0x4BEA00], esi
     Process::write(process, 0x41F8C7, rb2[1]).unwrap();
     Process::write(process, 0x41F8DD, rb1[16]).unwrap();   // -> mov [0x4BEA00], 0x1
-    Process::write(process, 0x41F8E1, rb2[2]).unwrap();
-    Process::write(process, 0x41F8E5, 0x0 as u32).unwrap();
+    Process::write(process, 0x41F8E1, 0x1004B as u32).unwrap();
+    Process::write(process, 0x41F8E5, 0x0 as u16).unwrap();
     Process::write(process, 0x41F901, rb1[17]).unwrap();   // -> mov [0x4BEA00], eax
     Process::write(process, 0x41F905, rb2[0]).unwrap();    // -> nop
     Process::write(process, 0x41F92F, rb1[18]).unwrap();   // -> mov [0x4BEA00], edx
@@ -100,12 +100,12 @@ pub unsafe extern "stdcall" fn overwrite(process: &Process) -> Result<(), &'stat
     Process::write(process, 0x41FF8A, rb2[0]).unwrap();
     Process::write(process, 0x41FFEA, rb1[13]).unwrap();
     Process::write(process, 0x41FFEE, rb2[1]).unwrap();
-    Process::write(process, 0x41FFF0, 0x3 as u32).unwrap();
+    Process::write(process, 0x41FFF0, 0x3 as u8).unwrap();
     Process::write(process, 0x42E1EB, rb1[19]).unwrap();
     Process::write(process, 0x42E1EF, rb2[1]).unwrap();
     Process::write(process, 0x44152C, 0x90909090 as u32).unwrap();     // -> nop nop nop nop
-    Process::write(process, 0x44152C + 0x4, 0xEB90 as u32).unwrap();   // -> nop
-    Process::write(process, 0x44152C + 0x2, 0x2C as u32).unwrap();     // -> jmp 0x44155F
+    Process::write(process, 0x44152C + 0x4, 0xEB90 as u16).unwrap();   // -> nop
+    Process::write(process, 0x44152C + 0x2, 0x2C as u8).unwrap();     // -> jmp 0x44155F
 
     Ok(())
 }
