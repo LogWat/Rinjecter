7164f3cc0514fabfe96069946b1ca02d28188121
LogWat <Nk8877wpt+Karan@gmail.com>
2022-02-07 16:53:14 +0900

    不要なmsgbox呼び出し削除

--- src/dbg.rs
+++ src/dbg.rs
@@ -72,16 +72,3 @@ pub unsafe extern "stdcall" fn Get_Thread_Owner_PID(process: &Process) -> Result
 
     Ok(())
 }
-
-pub unsafe extern "stdcall" fn err_msgbox(text: String) {
-    let lp_text: Vec<u16> = text.encode_utf16().collect();
-    let caption = "⚠Error⚠\0".to_string();
-    let lp_caption: Vec<u16> = caption.encode_utf16().collect();
-
-    MessageBoxW(
-        std::ptr::null_mut(),
-        lp_text.as_ptr(),
-        lp_caption.as_ptr(),
-        MB_OK
-    );
-}
