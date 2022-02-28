59a79b8cda19dce0f9df40866d5111e515ee5a32
LogWat <Nk8877wpt+Karan@gmail.com>
2022-03-01 01:26:51 +0900

    デバッグ特権取得メソッド追加

--- dll2/src/dbg.rs
+++ dll2/src/dbg.rs
@@ -1,4 +1,18 @@
-use crate process::Process;
+use winapi::{
+    um::{
+        winnt::{
+            HANDLE, TOKEN_ADJUST_PRIVILEGES, TOKEN_QUERY, TOKEN_PRIVILEGES, SE_PRIVILEGE_ENABLED
+        },
+        processthreadsapi, winbase, securitybaseapi,
+    },
+    shared::minwindef::{
+        DWORD,
+    },
+};
+
+use std::{mem};
+use crate::process::Process;
+use crate::ffi_helpers;
 
 #[repr(C)]
 pub struct Debugger {
@@ -13,5 +27,53 @@ impl Debugger {
             process: Process::empty(),
             isDebuggerAttached: false,
         }
+    }
+
+    pub fn set_privilege(&self) -> Result<(), &'static str> {
+
+        let mut token: HANDLE = std::ptr::null_mut();
+        if unsafe {
+            processthreadsapi::OpenProcessToken(
+                self.process.handle,
+                TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY,
+                &mut token
+            )
+        } == 0 {
+            return Err("Failed to open process token.");
+        }
+
+        if token == std::ptr::null_mut() {
+            return Err("Failed to open process token.");
+        }
+
+        let mut tkp: TOKEN_PRIVILEGES = unsafe { mem::zeroed() };
+        let privilege = ffi_helpers::win32_to_utf16("seDebugPrivilege");
+        if unsafe {
+            winbase::LookupPrivilegeValueW(
+                0 as *mut _,
+                privilege.as_ptr(),
+                &mut tkp.Privileges[0].Luid
+            )
+        } == 0 {
+            return Err("Failed to lookup privilege value.");
+        }
+
+        tkp.PrivilegeCount = 1;
+        tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+        if unsafe {
+            securitybaseapi::AdjustTokenPrivileges(
+                token,
+                0,
+                &mut tkp,
+                0,
+                0 as *mut _,
+                0 as *mut _
+            )
+        } == 0 {
+            return Err("Failed to adjust token privileges.");
+        }
+
+        Ok(())
     }
 }
